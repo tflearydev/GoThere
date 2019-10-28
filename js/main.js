fd401770f2fc14c59a89c03c62ba8506bda0122a
Tray Fleary <tflearydev@gmail.com>
2019-10-28 16:00:34 -0400

    completed login and register web pages, waiting for mobile responsive wireframes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function show(elementID) {
+    var ele = document.getElementById(elementID);
+    if (!ele) {
+        alert("no such element");
+        return;
+    }
+    var pages = document.getElementsByClassName('page');
+    for(var i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+    ele.style.display = 'block';
+}
