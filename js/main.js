d16738885134472e7baec16a9c8105baf55384df
Tray Fleary <tflearydev@gmail.com>
2019-10-30 13:58:27 -0400

    complete login page web and mobile responsive for iphone, android, and tablet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// transition from login page to register page without reloading the document
 function show(elementID) {
     var ele = document.getElementById(elementID);
     if (!ele) {
@@ -10,3 +11,22 @@ function show(elementID) {
     }
     ele.style.display = 'block';
 }
+
+//side navigation js
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    // document.getElementById("main").style.marginLeft = "250px";
+    // document.querySelector("body").style.backgroundColor = "rgba(0,0,0,0.4)";
+
+  
+    // planned to make entire page slide right but it compromised style on mobile
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("main").style.marginLeft= "0";
+    // document.body.style.backgroundColor = "white";
+ 
+     // planned to make entire page slide right but it compromised style on mobile
+  }
+
